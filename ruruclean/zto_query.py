da85da1ede0d88f099ab4e766e63b6fd6225413d
shengshuaijohnson <120559187@qq.com>
2017-04-22 15:09:23 +0800

    update

--- ruruclean/zto_query.py
+++ ruruclean/zto_query.py
@@ -1,8 +1,8 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Author: Yixuan Zhao <johnsonqrr (at) gmail.com>
-# 测例：正常：532499544594,418766752579;无进展：982202832284;国际：312574842215
-
+# 数据测例：正常：532499544594,418766752579;无进展：982202832284;国际：312574842215; 无任何信息，返回空：42174038718
+# 爬取过程中还发现了无效数据如： 4121å22092612 TAT   还有一些带\n的单号
 
 import json
 import requests
@@ -18,7 +18,6 @@ header = {
 'Connection':'keep-alive',
 'Content-Length':'20',
 'Content-Type':'application/x-www-form-urlencoded',
-# 'Cookie':'ASP.NET_SessionId=34osrid5tfrdomtgfabftc2e; UserBills=["982202832284","419301776745","509985361111","128663450338"]; Hm_lvt_a568d570cd1ef66819cda1e7d5cbbe08=1492684486; Hm_lpvt_a568d570cd1ef66819cda1e7d5cbbe08=1492685053; Hm_lvt_53a93979e64ab8e76c06653f6830c385=1492684486; Hm_lpvt_53a93979e64ab8e76c06653f6830c385=1492685053',
 'Cookie' : 'ASP.NET_SessionId=34osrid5tfrdomtgfabftc2e; UserBills=["982202832284","419301776745","509985361111","128663450338","405887775211","532122677728","408065610697","419334339883","415669707661"]; Hm_lvt_a568d570cd1ef66819cda1e7d5cbbe08=1492684486; Hm_lpvt_a568d570cd1ef66819cda1e7d5cbbe08=1492687476; Hm_lvt_53a93979e64ab8e76c06653f6830c385=1492684486; Hm_lpvt_53a93979e64ab8e76c06653f6830c385=1492687476',
 'Host':'www.zto.com',
 'Origin':'http://www.zto.com',
@@ -32,7 +31,8 @@ header = {
 url = 'http://www.zto.com/GuestService/BillNew'
 
 
-def write_to_file(item, output_file='output.txt'):
+
+def write_to_file(item, output_file='output.txt'):  # 为了便于数据库储存，使用json格式。如果要保存到csv，需要使用pandas的write_to_csv
     with open(output_file, 'a') as f:
         a = json.dumps(item, ensure_ascii=False)
         print a
@@ -40,11 +40,12 @@ def write_to_file(item, output_file='output.txt'):
         f.write('\n')
 
 def process_single_bill(bill_id):
-    # TODO:2秒间隔下爬取时间也会有3小时，考虑到学校网络不稳定，可以作暴力一点的断点续传设计
     # 快递状态大致分类：未寄件（本批数据较老，不会有这种情况）；已寄件未签收（本批数据也应该不会有）；已寄件且已签收；海外快递；无运单信息。
     for _ in range(3):
         try:
+            print 'begin request'
             res = requests.post(url, data={'txtBill': bill_id}, headers=header)
+            print 'end request'
             break
         except requests.exceptions.ConnectionError:
             print 'Oops!ConnectionError:('
@@ -58,15 +59,16 @@ def process_single_bill(bill_id):
     item = {
         u'bill_id'               : bill_id,
         u'origin_city'           : None,             # 印象中某些情况下数据库对于这个None不太友好？不合适的话可以改成''
-        u'origin_post_part'           : None,
+        u'origin_post_part'      : None,
         u'destination_city'      : None,
-        u'destination_post_part'      : None,
+        u'destination_post_part' : None,
         u'delivery_date'         : None,
         u'arrival_date'          : None,
         u'postman_phonenumber'   : None,
         u'status'                : 'OK',                    
     }
     dom = lxml.html.fromstring(res.content)
+    # print res.content
     state_node = dom.xpath('//div[@class="state"]')[0]
     steps = state_node.xpath('.//li')
     if not steps:              
@@ -91,42 +93,62 @@ def process_single_bill(bill_id):
         item[u'destination_post_part'] = destination_post_part
         item[u'arrival_date'] = steps[-1].xpath('./div[@class="time"]/text()')[0]    
 
-        # 地点和时间提取完，接下来检查是否有送货员电话，为了便捷起见，采取较直接的方式获取
-        # 判断方法有：匹配[数字]；查找'正在派件'；直接取第二个节点等。
+        # 地点和时间提取完，接下来检查是否有送货员电话，判断方法有很多种，这里采用关键字匹配和数字格式匹配混合
         for node in steps:
             text = u''.join(node.xpath('.//text()')).strip()
             if u'正在派件' in text:
-                phone = re.findall('\[(\d+)\]', text)
+                phone = re.findall('\[(\d+)\]', text)           # 马亮[15852172024] 
                 if phone:
                     item[u'postman_phonenumber'] = phone[0]
                 break
 
         write_to_file(item)
+    return True
 
+   
+def check_if_crawled(bill_number, file_name):
+    with open(file_name, 'r') as f:
+        success_number = [line.strip() for line in f]
+    return bill_number in success_number
 
+def mark_as_crawled(bill_number, file_name):
+    with open(file_name,'a') as f:
+        f.write(bill_number)
+        f.write('\n')
 
-    # 为了便于数据库储存，使用json格式。如果要保存到csv，需要使用pandas
-
-def test(gap, times):        
-    # 最开始测试gap时process方法里的语句只有发送request和打印len(content)，之后的若干处理都还没写
-    # 测试结果：无间隔20次ban,持续10~20分钟左右;3秒间隔连续50次未被ban;2秒间隔200次未被ban
-    for count in range(times):
-        print count
-        process_single_bill('532499544594')  
-        time.sleep(gap)         
 
 def main():
+    # 由于校园网不稳定以及中通服务器经常返回异常网页，做了一个土制的断点续传记录失败系统
     file_name = 'zto_order_id.csv'
     with open(file_name) as f:
         zto_numbers = [line.strip() for line in f]
     zto_numbers = list(set(zto_numbers))        # 竟然有数据重复，我太TM机智了,据我观察是文件头尾的数据有重复
-    counte = 0
+    count = 0
     for bill_number in zto_numbers:
-        print counte
-        counte += 1
-        process_single_bill(bill_number)
-        time.sleep(2)
+        if check_if_crawled(bill_number, 'success.txt') or check_if_crawled(bill_number, 'failed.txt'):
+            continue
+        # 起初只检查success.txt，后来因为多次断网重启并且有些单号是必定失败，效率起见改成两个都检查，爬完可以改回来
+        print count
+        count += 1
+        try:
+            if process_single_bill(bill_number):            # 之前没if导致多了4个连接error数据的差别，爬完以后再检查修正一下
+                mark_as_crawled(bill_number, 'success.txt')
+        except Exception, e:
+            print bill_number, e
+            mark_as_crawled(bill_number, 'failed.txt')
+        print 'sleep'
+        time.sleep(1.5)
 # test(0, 1)
 
 if __name__ == '__main__':
     main()
+
+
+
+
+# def test(gap, times):        
+#     # 起初测试gap时process方法里的语句只有最简单的发送request和打印len(content)
+#     for count in range(times):
+#         print count
+#         process_single_bill('532499544594')  
+#         time.sleep(gap)      
